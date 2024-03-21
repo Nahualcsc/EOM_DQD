33ffa4b7464dab33c7fc221ca4c0870a7464ddd8
Nahual Sobrino <33580847+Nahualcsc@users.noreply.github.com>
2024-03-21 20:44:44 +0100

    Update main.py
    Included Animation options

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import numpy as np
 from scipy import integrate
 from tqdm import tqdm
+import os
 
 from inputs import *
 from EOM import *
@@ -11,13 +12,14 @@ from generate_plots import *
 #######################################################
 Uave = 0.5*(U1+U2)
 
-if U12<=U1 and U12<=U2: ##REGIME 1
-    print('REGIME 1')
-elif U12>U1 and Uave>U12: #REGIME 2
-    print('REGIME 2')
-elif Uave>=U1 and U12>=Uave: #REGIME 3
-    print('REGIME 3')
-
+try:
+    if U12<=U1 and U12<=U2: ##REGIME 1
+        print('REGIME 1')
+    elif U12>U1 and Uave>U12: #REGIME 2
+        print('REGIME 2')
+    elif Uave>=U1 and U12>=Uave: #REGIME 3
+        print('REGIME 3')
+except: pass
 list_file = []
 
 if calculate_densities_vs_gate:
@@ -28,7 +30,7 @@ if calculate_densities_vs_gate:
         EOM = Equations_of_motion( veq+delta_v/2,veq-delta_v/2,V,T+0.5*delta_T,T-0.5*delta_T,U1,U2,U12,gamma1,gamma2)
         I_EOM0,Q_EOM0 = EOM.Current(EOM.n1,EOM.n2)
         if delta_T!=0.:efficiency_EOM0, output_power_EOM0 = EOM.efficiency(I_EOM0,Q_EOM0)
-        else: efficiency_EOM0, output_power_EOM0 = 0.,0.,0.
+        else: efficiency_EOM0, output_power_EOM0 = 0.,0.
         if compute_EOMH:
             dens_EOMH = optimize.root( EOM.solve_H, [0.5*dens_GCE[0],0.5*dens_GCE[1]], method='hybr', tol=1.e-12).x
             I_EOMH,Q_EOMH = EOM.Current_EOMH(dens_EOMH[0],dens_EOMH[1])
@@ -71,28 +73,54 @@ if calculate_spectral_function:
 ###############################################
 ###############################################
 
-
-if colormap_spectral_function:
+def compute_Aw(w_range,U12_range,V,v1,v2):
     A = np.zeros((len(U12_range), len(w_range)))
-    for i, U12 in enumerate(tqdm(U12_range)):
-        v1 = -0.5*U1-U12
-        v2 = -0.5*U2-U12
+    output = 'U1_{}_U2_{}_V_{}_T_{}_g1_{}_g2_{}_dT_{}'.format(U1, U2, V, T, gamma1, gamma2,delta_T)
+    for i, U12 in enumerate(U12_range):
+        if colormap_spectral_function:
+            v1 = -0.5*U1-U12
+            v2 = -0.5*U2-U12
         for j, w in enumerate(w_range):
             EOM = Equations_of_motion(v1,v2,V,T+0.5*delta_T,T-0.5*delta_T,U1,U2,U12,gamma1,gamma2)
             A_1 = EOM.Ai(w,v1, v2, U1, U2, gamma1, gamma2,EOM.n1,EOM.n2)
             A_2 = EOM.Ai(w,v2, v1, U2, U1, gamma2, gamma1,EOM.n2,EOM.n1)
             A[i, j] = (A_1+A_2)
-    plot_colormap_spectral_function(w_range, U12_range, A)
+    return A
 
+if colormap_spectral_function:
+    output = 'U1_{}_U2_{}_T_{}_g1_{}_g2_{}_dT_{}'.format(U1, U2, T, gamma1, gamma2,delta_T)
+    A = compute_Aw(w_range,U12_range,V,0.,0.)
+    plot_colormap_spectral_function(w_range, U12_range, A,'','Aw',output)
+
+
+if movie_spectral_function_vary_V:
+    folder = 'movies'
+    output = 'U1_{}_U2_{}_dV_{}_T_{}_g1_{}_g2_{}_dT_{}_V_{}_to_{}'.format(U1, U2, delta_v, T, gamma1, gamma2,delta_T,V_range[0],V_range[-1])
+    for V in tqdm(V_range):
+        A = compute_Aw(w_range,U12_range,V,v1,v2)
+        title = '$V$ = '+str(round(V,4))
+        plot_colormap_spectral_function(w_range, U12_range, A, title,folder,str(V))
+    create_movies_colormap_spectral_function(output)
+
+if movie_spectral_function_vary_dv:
+    folder = 'movies'
+    output = 'U1_{}_U2_{}_V_{}_T_{}_g1_{}_g2_{}_dT_{}_dv_{}_to_{}'.format(U1, U2, V, T, gamma1, gamma2,delta_T,dv_range[0],dv_range[-1])
+    for delta_v in tqdm(dv_range):
+        v1 = v+0.5*delta_v
+        v2 = v -0.5*delta_v
+        A = compute_Aw(w_range,U12_range,V,v1,v2)
+        title = '$\\delta v$ = '+str(round(delta_v,4))
+        plot_colormap_spectral_function(w_range, U12_range, A, title,folder,str(delta_v))
+    create_movies_colormap_spectral_function(output)
 
 ###############################################
 ###############################################
-if colormaps_currents:
+def compute_olormaps_currents(v_range,V_range,U12):
     dens_0 =  np.zeros((len(V_range), len(v_range)))
     dens_1 =  np.zeros((len(V_range), len(v_range)))
     I_EOM0 =  np.zeros((len(V_range), len(v_range)))
     Q_EOM0 =  np.zeros((len(V_range), len(v_range)))
-    for i, V in enumerate(tqdm(V_range)):
+    for i, V in enumerate(V_range):
         for j, v in enumerate(v_range):
             EOM = Equations_of_motion(v+delta_v/2,v-delta_v/2, V, T + 0.5 * delta_T, T - 0.5 * delta_T, U1, U2, U12, gamma1, gamma2)
             dens = EOM.densities()
@@ -101,43 +129,108 @@ if colormaps_currents:
             I, Q= EOM.Current(EOM.n1,EOM.n2)
             I_EOM0[i, j] = I
             Q_EOM0[i, j] = Q
-    plot_colormaps_currents(v_range,V_range,dens_0,dens_1,I_EOM0,Q_EOM0)
+    return dens_0,dens_1,I_EOM0,Q_EOM0
+
+if colormaps_currents:
+    output = 'U1_{}_U2_{}_U12_{}_T_{}_g1_{}_g2_{}_dT_{}_dv_{}'.format(U1, U2, U12, T, gamma1, gamma2,delta_T,dv)
+    dens_0,dens_1,I_EOM0,Q_EOM0 = compute_olormaps_currents(v_range,V_range,U12)
+    plot_colormaps_currents(v_range,V_range,dens_0,dens_1,I_EOM0,Q_EOM0,'','colormaps',output)
+
+
+
+if movie_currents_vary_U12:
+    output = 'U1_{}_U2_{}_T_{}_DT_{}_g1_{}_g2_{}_dv_{}_U12_{}_to_{}'.format(U1, U2, T, delta_T, gamma1, gamma2,delta_v,U12_range[0],U12_range[-1])
+    folder = 'movies'
+    for U12 in tqdm(U12_range):
+        dens_0,dens_1,I_EOM0,Q_EOM0 = compute_olormaps_currents(v_range,V_range,U12)
+        title = '$U_{12}$ = '+str(round(U12,3))
+        plot_colormaps_currents(v_range,V_range,dens_0,dens_1,I_EOM0,Q_EOM0,title,folder,str(U12))
+    create_movies_colormap_currents(output)
 
 
 ###############################################
 ###############################################
 
-if colormap_efficiency:
+def compute_colormap_efficiency(v_range,V_range,delta_T):
     eficciency_norm_EOM0 = np.zeros((len(V_range), len(v_range)))  # Initialize the efficiency array
-    for i, V in enumerate(tqdm(V_range)):
+    for i, V in enumerate(V_range):
         for j, v in enumerate(v_range):
             v1 = v + delta_v
             v2 = v - delta_v
             EOM = Equations_of_motion(v1, v2, V, T + 0.5 * delta_T, T - 0.5 * delta_T, U1, U2, U12, gamma1, gamma2)
             I, Q = EOM.Current(EOM.n1, EOM.n2)
             eficciency_norm_EOM0[i, j], _ = EOM.efficiency(I, Q)
-    plot_colormap_efficiency(v_range,V_range,eficciency_norm_EOM0)
+    return eficciency_norm_EOM0
+
+if colormap_efficiency:
+    output = 'U1_{}_U2_{}_U12_{}_T_{}_g1_{}_g2_{}_dT_{}_dv_{}'.format(U1, U2, U12, T, gamma1, gamma2,delta_T,dv)
+    eficciency_norm_EOM0 = compute_colormap_efficiency(v_range,V_range,delta_T)
+    plot_colormap_efficiency(v_range,V_range,eficciency_norm_EOM0,V,U12,'','colormaps',output)
+
+
+if movie_efficiency_vary_DT:
+    output = 'U1_{}_U2_{}_U12_{}_T_{}_g1_{}_g2_{}_dv_{}_DT_{}_to_{}'.format(U1, U2, U12, T, gamma1, gamma2,delta_v,DT_range[0],DT_range[-1])
+    folder = 'movies'
+    for delta_T in tqdm(DT_range):
+        eficciency_norm_EOM0 = compute_colormap_efficiency(v_range,V_range,delta_T)
+        title = '$\\Delta T$ = '+str(round(delta_T,4))
+        plot_colormap_efficiency(v_range,V_range,eficciency_norm_EOM0,delta_T,title,folder,'efficiency_'+str(delta_T))
+    create_movies_colormap_efficiency(output)
 
 ###############################################
 ###############################################
 
-if colormap_residues_stability_diagram:
+def compute_SD(v1_range,v2_range,V,U12,delta_T):
     Z = np.zeros((len(v1_range), len(v2_range), 6))  # For z1 to z6
     I_values = np.zeros((len(v1_range), len(v2_range)))
     Q_values = np.zeros((len(v1_range), len(v2_range)))
     nsum = np.zeros((len(v1_range), len(v2_range)))
-    for i, v1 in enumerate(tqdm(v1_range)):
+    for i, v1 in enumerate(v1_range):
         for j, v2 in enumerate(v2_range):
-            EOM = Equations_of_motion(v1, v2, V, T + 0.5 * delta_T, T - 0.5 * delta_T, U1, U2, U12, gamma1, gamma2)
+            EOM = Equations_of_motion(v1, v2, V, T + 0.5*delta_T, T -0.5*delta_T, U1, U2, U12, gamma1, gamma2)
             nsum[i,j] = (2*EOM.n1+5*EOM.n2)
             Z[i, j, :] = EOM.numerators(v2, v1, U2, U1, gamma2, gamma1, EOM.n2, EOM.n1)
             I, Q = EOM.Current(EOM.n1, EOM.n2)
             I_values[i, j] = I*100
             Q_values[i, j] = Q*100
-    plot_colormap_residues_stability_diagram(v1_range, v2_range, I_values, Q_values,Z, nsum)
+    return I_values, Q_values,Z, nsum
 
+
+if colormap_SD:
+    output = 'U1_{}_U2_{}_U12_{}_T_{}_g1_{}_g2_{}_dT_{}_V_{}'.format(U1, U2, U12, T, gamma1, gamma2,delta_T,V)
+    I_values, Q_values,Z, nsum = compute_SD(v1_range,v2_range,V,U12,delta_T)
+    plot_colormap_SD(v1_range, v2_range, I_values, Q_values,Z, nsum,V,U12,delta_T,'','colormaps',output)
+
+
+if movie_SD_vary_V:
+    folder = 'movies'
+    output = 'U1_{}_U2_{}_U12_{}_T_{}_g1_{}_g2_{}_dT_{}_V_{}_to_{}'.format(U1, U2, U12, T, gamma1, gamma2,delta_T,V_range[0],V_range[-1])
+    for V in tqdm(V_range):
+        I_values, Q_values,Z, nsum = compute_SD(v1_range,v2_range,V,U12,delta_T)
+        title = 'V = '+str(round(V,4))
+        plot_colormap_SD(v1_range, v2_range, I_values, Q_values,Z, nsum,V,U12,delta_T,title,folder,str(V))
+    create_movies_colormap_SD(output)
+
+if movie_SD_vary_U12:
+    folder = 'movies'
+    output = 'U1_{}_U2_{}_T_{}_g1_{}_g2_{}_V_{}_dT_{}_U12_{}_to_{}'.format(U1, U2, T, gamma1, gamma2,V,delta_T,U12_range[0],U12_range[-1])
+    for U12 in tqdm(U12_range):
+        I_values, Q_values,Z, nsum = compute_SD(v1_range,v2_range,V,U12,delta_T)
+        title = '$U12$ = '+str(round(U12,2))
+        plot_colormap_SD(v1_range, v2_range, I_values, Q_values,Z, nsum,V,U12,delta_T,title,folder,str(U12))
+    create_movies_colormap_SD(output)
+
+if movie_SD_vary_DT:
+    folder = 'movies'
+    output = 'U1_{}_U2_{}_T_{}_g1_{}_g2_{}_V_{}_U12_{}_dT_{}_to_{}'.format(U1, U2, T, gamma1, gamma2,V,U12,DT_range[0],DT_range[-1])
+    for delta_T in tqdm(DT_range):
+        I_values, Q_values,Z, nsum = compute_SD(v1_range,v2_range,V,U12,delta_T)
+        title = '$\\Delta T$ = '+str(round(delta_T,4))
+        plot_colormap_SD(v1_range, v2_range, I_values, Q_values,Z, nsum,V,U12,delta_T,title,folder,str(delta_T))
+    create_movies_colormap_SD(output)
 
 ###############################################
 ###############################################
 
 print('DONE')
+
